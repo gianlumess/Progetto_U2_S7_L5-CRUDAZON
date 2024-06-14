2b813b4ae776b37e0de757eeabaeaf0408dec887
Gianluca Messina <gianluca.messina8201@gmail.com>
2024-06-14 14:03:10 +0200

    backoffice  wip

---
+++ backoffice.js
@@ -0,0 +1,35 @@
+const params = new URLSearchParams(window.location.search);
+const pexelId = params.get("productId");
+
+const APIKEY =
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjZiZmZmNDdjMjM5YzAwMTUyZjRiNzQiLCJpYXQiOjE3MTgzNTM5MDgsImV4cCI6MTcxOTU2MzUwOH0.YxOTllrumawWZJ4LtdXWOHBZKu9J2pg4-y4aQ09JeiQ";
+const headers = {
+  Authorization: APIKEY,
+};
+
+console.log(productId);
+//se l'id sara presente utilizzeremo il metodo PUT per modificare il prodotto, altrimenti utilizzeremo il metodo POST per crearne uno nuvo
+const method = id ? "PUT" : "POST";
+
+const URL = id
+  ? "https://striveschool-api.herokuapp.com/api/product/" + id
+  : "https://striveschool-api.herokuapp.com/api/product/";
+
+window.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("form");
+  const paragraph = document.querySelector("h6");
+
+  if (id) {
+    paragraph.innerText = "Modifica prodotto";
+    const btnContainer = document.getElementById("button-container");
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("btn", "btn-outline-danger", "btn-sm");
+    deleteBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                                class="bi bi-trash3" viewBox="0 0 16 16">
+                                <path
+                                    d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
+                            </svg>`;
+
+    btnContainer.appendChild(deleteBtn);
+  }
+});
